af9cad7c103fa11a5c52b51f0040ba57e75f895e
Zang MingJie <zealot0630@gmail.com>
2018-12-26 18:28:43 +0800

    test: use ava snapshots

--- test/index.js
+++ test/index.js
@@ -1,11 +1,11 @@
 /* eslint-env es6 */
+import { promises as fs } from 'fs';
 import test from 'ava';
 import path from 'path';
 import rimraf from 'rimraf';
 import FaviconsWebpackPlugin from '..';
 import denodeify from 'denodeify';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
-import dircompare from 'dir-compare';
 import packageJson from '../package.json';
 
 const webpack = denodeify(require('webpack'));
@@ -13,7 +13,6 @@ const readFile = denodeify(require('fs').readFile);
 const writeFile = denodeify(require('fs').writeFile);
 const mkdirp = denodeify(require('mkdirp'));
 
-const compareOptions = {compareSize: true};
 let outputId = 0;
 const LOGO_PATH = path.resolve(__dirname, 'fixtures/logo.png');
 
@@ -24,10 +23,30 @@ function baseWebpackConfig (plugin) {
     devtool: 'eval',
     entry: path.resolve(__dirname, 'fixtures/entry.js'),
     output: {
-      path: path.resolve(__dirname, '../dist', 'test-' + (outputId++))
+      path: path.resolve(__dirname, '../dist', 'test-' + outputId++)
     },
     plugins: [].concat(plugin)
   };
+}
+
+async function readFiles (directory) {
+  async function readFilesInner (result, base, rel) {
+    const filenames = await fs.readdir(path.join(base, rel));
+    for (const filename of filenames) {
+      const relpath = path.join(rel, filename);
+      const fullpath = path.join(base, relpath);
+      const stat = await fs.stat(fullpath);
+      if (stat.isDirectory()) await readFilesInner(result, base, relpath);
+      else if (stat.isFile()) result[relpath] = await fs.readFile(fullpath);
+      else if (stat.isSymbolicLink()) {
+        result[relpath] = { link: await fs.readlink(fullpath) };
+      } else result[relpath] = { type: 'unknown' };
+    }
+  }
+
+  const result = {};
+  await readFilesInner(result, directory, './');
+  return result;
 }
 
 test('should throw error when called without arguments', async t => {
@@ -52,45 +71,43 @@ test('should take an object with just the logo as argument', async t => {
 });
 
 test('should generate the expected default result', async t => {
-  const stats = await webpack(baseWebpackConfig(new FaviconsWebpackPlugin({
-    logo: LOGO_PATH
-  })));
-  const outputPath = stats.compilation.compiler.outputPath;
-  const expected = path.resolve(__dirname, 'fixtures/expected/default');
-  const compareResult = await dircompare.compare(outputPath, expected, compareOptions);
-  const diffFiles = compareResult.diffSet.filter((diff) => diff.state !== 'equal');
-  t.is(diffFiles[0], undefined);
+  const stats = await webpack(
+    baseWebpackConfig(
+      new FaviconsWebpackPlugin({
+        logo: LOGO_PATH
+      })
+    )
+  );
+  t.snapshot(await readFiles(stats.compilation.compiler.outputPath));
 });
 
 test('should generate a configured JSON file', async t => {
-  const stats = await webpack(baseWebpackConfig(new FaviconsWebpackPlugin({
-    logo: LOGO_PATH,
-    emitStats: true,
-    persistentCache: false,
-    statsFilename: 'iconstats.json'
-  })));
-  const outputPath = stats.compilation.compiler.outputPath;
-  const expected = path.resolve(__dirname, 'fixtures/expected/generate-json');
-  const compareResult = await dircompare.compare(outputPath, expected, compareOptions);
-  const diffFiles = compareResult.diffSet.filter((diff) => diff.state !== 'equal');
-  t.is(diffFiles[0], undefined);
+  const stats = await webpack(
+    baseWebpackConfig(
+      new FaviconsWebpackPlugin({
+        logo: LOGO_PATH,
+        emitStats: true,
+        persistentCache: false,
+        statsFilename: 'iconstats.json'
+      })
+    )
+  );
+  t.snapshot(await readFiles(stats.compilation.compiler.outputPath));
 });
 
 test('should work together with the html-webpack-plugin', async t => {
-  const stats = await webpack(baseWebpackConfig([
-    new FaviconsWebpackPlugin({
-      logo: LOGO_PATH,
-      emitStats: true,
-      statsFilename: 'iconstats.json',
-      persistentCache: false
-    }),
-    new HtmlWebpackPlugin()
-  ]));
-  const outputPath = stats.compilation.compiler.outputPath;
-  const expected = path.resolve(__dirname, 'fixtures/expected/generate-html');
-  const compareResult = await dircompare.compare(outputPath, expected, compareOptions);
-  const diffFiles = compareResult.diffSet.filter((diff) => diff.state !== 'equal');
-  t.is(diffFiles[0], undefined);
+  const stats = await webpack(
+    baseWebpackConfig([
+      new FaviconsWebpackPlugin({
+        logo: LOGO_PATH,
+        emitStats: true,
+        statsFilename: 'iconstats.json',
+        persistentCache: false
+      }),
+      new HtmlWebpackPlugin()
+    ])
+  );
+  t.snapshot(await readFiles(stats.compilation.compiler.outputPath));
 });
 
 test('should not recompile if there is a cache file', async t => {
@@ -105,7 +122,11 @@ test('should not recompile if there is a cache file', async t => {
 
   // Bring cache file in place
   const cacheFile = 'icons-366a3768de05f9e78c392fa62b8fbb80/.cache';
-  const cacheFileExpected = path.resolve(__dirname, 'fixtures/expected/from-cache/', cacheFile);
+  const cacheFileExpected = path.resolve(
+    __dirname,
+    'fixtures/expected/from-cache/',
+    cacheFile
+  );
   const cacheFileDist = path.resolve(__dirname, options.output.path, cacheFile);
   await mkdirp(path.dirname(cacheFileDist));
   const cache = JSON.parse(await readFile(cacheFileExpected));
@@ -113,10 +134,5 @@ test('should not recompile if there is a cache file', async t => {
   await writeFile(cacheFileDist, JSON.stringify(cache));
 
   const stats = await webpack(options);
-  const outputPath = stats.compilation.compiler.outputPath;
-  const expected = path.resolve(__dirname, 'fixtures/expected/from-cache');
-  const compareResult = await dircompare.compare(outputPath, expected, compareOptions);
-  const diffFiles = compareResult.diffSet.filter((diff) => diff.state !== 'equal');
-  t.is(diffFiles[0], undefined);
+  t.snapshot(await readFiles(stats.compilation.compiler.outputPath));
 });
-
